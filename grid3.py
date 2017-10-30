c8b28b07d12fcd9c65b74eb93136745aecba1993
bruno <bsp@icomp.ufam.edu.br>
2017-10-30 00:23:07 -0400

    Desenhando quadrado azul

--- grid3.py
+++ grid3.py
@@ -20,58 +20,31 @@ class Grid:
                 self.w.create_line([(i, 0), (i,h)]) 
             for	i in range(0, w,cell_w):  
                 self.w.create_line([(0,i),(w,i)])
-	
+
+    def draw_square(self, lin, col):
+        x = col * self.cell_h
+        y = lin * self.cell_w
+        return self.w.create_rectangle(x, y, x+60, y+60, fill='blue', outline='')
+
     def draw_circle(self, lin, col): 
         x = col	* self.cell_h
         y = lin	* self.cell_w  
-        return	self.w.create_oval(x + 10, y + 10,
-                x + self.cell_w	- 10, y + self.cell_h - 10, 
+        return	self.w.create_oval(x + 60, y + 60,
+                x + self.cell_w	- 60, y + self.cell_h - 60, 
                 fill = 'blue', outline ='')
-class Quadrado(object):
-    
-    def __init__(self, w, x = 10, y = 10, lado = 100, cor = 'blue'):
-        self.w = w
-        self.x = x
-        self.y = y
-        self.lado = lado
-        self.cor = cor
-        self.id = -1
-    
-    def desenhar(self):
-        self.id = self.w.create_rectangle(self.x, self.y, 
-                                          self.x + self.lado, 
-                                          self.y + self.lado,
-                                          outline = self.cor)
 
-class Circulo(object):
-    
-    def __init__(self, w, x = 10, y = 10, raio = 100, cor = 'blue'):
-        self.w = w
-        self.x = x
-        self.y = y
-        self.raio = raio
-        self.cor = cor
-        self.id = -1
-    
-    def desenhar(self):
-        self.id = self.w.create_oval(self.x - self.raio, 
-                                     self.y - self.raio, 
-                                     self.x + self.raio, 
-                                     self.y + self.raio,
-                                     outline = self.cor)
+
 
 if __name__	== '__main__':	  
     m = Tk() 
     m.title('Grid World')	  
     grid =  Grid(m,5,5,cell_h = 60, cell_w = 60)
     
-    w = Canvas(m, width=400, height=400)
+    w = Canvas(m, width=40, height=0)
     w.pack()
+    
 
-   q = Quadrado(w, 10, 10, 10, 'green')
-    q.desenhar()
-
-#    c = Circulo(w, 10, 10, 20, 'blue')
-#    c.desenhar()
+    grid.draw_square(0,0)
 
     m.mainloop()
+
